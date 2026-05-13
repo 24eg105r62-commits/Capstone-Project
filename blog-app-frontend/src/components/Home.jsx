cc430f217f31f0ae80c13a2bab037ddfff5c1430
Nithish Ganji <nithish@Nithishs-MacBook-Air.local>
2026-05-13 13:54:08 +0530

    first commit

---
+++ blog-app-frontend/src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router";
+
+function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/articles");
+        const data = await response.json();
+        setArticles(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest Articles</h1>
+      <div>
+        {articles.map((article) => (
+          <div key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.summary}</p>
+            <Link to={`/article/${article.id}`}>Read More</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
