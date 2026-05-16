5d8052a9b5632fa2111369481029a930231b1345
Nithish Ganji <nithish@Nithishs-MacBook-Air.local>
2026-05-16 10:44:02 +0530

    latest commit

--- blog-app-backend/APIs/CommonAPI.js
+++ blog-app-backend/APIs/CommonAPI.js
@@ -1,4 +1,3 @@
-
 import exp from "express";
 import { UserModel } from "../models/UserModel.js";
 import { hash, compare } from "bcryptjs";
@@ -13,49 +12,53 @@ import cloudinary from "../config/cloudinary.js";
 config();
 
 //Route for register
-commonApp.post("/users", upload.single("profileImageUrl"), async (req, res,next) => {
-  let cloudinaryResult;
-  try {
-    let allowedRoles = ["USER", "AUTHOR"];
-    //get user from req
-    const newUser = req.body;
-    console.log(newUser);
-    console.log(req.file);
+commonApp.post(
+  "/users",
+  upload.single("profileImageUrl"),
+  async (req, res, next) => {
+    let cloudinaryResult;
+    try {
+      let allowedRoles = ["USER", "AUTHOR"];
+      //get user from req
+      const newUser = req.body;
+      console.log(newUser);
+      console.log(req.file);
 
-    //check role
-    if (!allowedRoles.includes(newUser.role)) {
-      return res.status(400).json({ message: "Invalid role" });
+      //check role
+      if (!allowedRoles.includes(newUser.role)) {
+        return res.status(400).json({ message: "Invalid role" });
+      }
+
+      //Upload image to cloudinary from memoryStorage
+      if (req.file) {
+        cloudinaryResult = await uploadToCloudinary(req.file.buffer);
+      }
+
+      // console.log("cloudinaryResult", cloudinaryResult);
+      //add CDN link(secure_url) of image to newUserObj
+      newUser.profileImageUrl = cloudinaryResult?.secure_url;
+
+      //run validators manually
+      //hash password and replace plain with hashed one
+      newUser.password = await hash(newUser.password, 12);
+
+      //create New user document
+      const newUserDoc = new UserModel(newUser);
+
+      //save document
+      await newUserDoc.save();
+      //send res
+      res.status(201).json({ message: "User created" });
+    } catch (err) {
+      console.log("err is ", err);
+      //delete image from cloudinary
+      if (cloudinaryResult.public_id) {
+        await cloudinary.uploader.destroy(cloudinaryResult.public_id);
+      }
+      next(err);
     }
-
-    //Upload image to cloudinary from memoryStorage
-    if (req.file) {
-      cloudinaryResult = await uploadToCloudinary(req.file.buffer);
-    }
-
-    // console.log("cloudinaryResult", cloudinaryResult);
-    //add CDN link(secure_url) of image to newUserObj
-    newUser.profileImageUrl = cloudinaryResult?.secure_url;
-
-    //run validators manually
-    //hash password and replace plain with hashed one
-    newUser.password = await hash(newUser.password, 12);
-
-    //create New user document
-    const newUserDoc = new UserModel(newUser);
-
-    //save document
-    await newUserDoc.save();
-    //send res
-    res.status(201).json({ message: "User created" });
-  } catch (err) {
-    console.log("err is ", err);
-    //delete image from cloudinary
-    if (cloudinaryResult.public_id) {
-      await cloudinary.uploader.destroy(cloudinaryResult.public_id);
-    }
-    next(err);
-  }
-});
+  },
+);
 
 //Route for Login(USER, AUTHOR and ADMIN)
 commonApp.post("/login", async (req, res) => {
@@ -93,8 +96,8 @@ commonApp.post("/login", async (req, res) => {
   //set token to res header as httpOnly cookie
   res.cookie("token", signedToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
   });
   //remove password from user document
   let userObj = user.toObject();
@@ -109,48 +112,63 @@ commonApp.get("/logout", (req, res) => {
   //delete token from cookie storage
   res.clearCookie("token", {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
   });
   //send res
   res.status(200).json({ message: "Logout success" });
 });
 
 //Page refresh
-commonApp.get("/check-auth", verifyToken("USER", "AUTHOR", "ADMIN"), (req, res) => {
-  res.status(200).json({
-    message: "authenticated",
-    payload: req.user,
-  });
-});
+commonApp.get(
+  "/check-auth",
+  verifyToken("USER", "AUTHOR", "ADMIN"),
+  (req, res) => {
+    res.status(200).json({
+      message: "authenticated",
+      payload: req.user,
+    });
+  },
+);
 
 //change password
-commonApp.put("/password",verifyToken("USER","AUTHOR","ADMIN"),async(req,res)=>{
+commonApp.put(
+  "/password",
+  verifyToken("USER", "AUTHOR", "ADMIN"),
+  async (req, res) => {
     //get current and new password from user
-    const {currentPassword,newPassword}=req.body
+    const { currentPassword, newPassword } = req.body;
     //check current password and new password are same
-    if(currentPassword===newPassword){
-        return res.status(400).json({message:"passwords are same.please give other password"})
+    if (currentPassword === newPassword) {
+      return res
+        .status(400)
+        .json({ message: "passwords are same.please give other password" });
     }
     //get current password of user/admin/author
-    const user=await UserModel.findById(req.user.id)
+    const user = await UserModel.findById(req.user.id);
 
     //check the curent password of req and user are not same
-    let isMatched=await compare(currentPassword,user.password)
-    if(!isMatched){
-        return res.status(400).json({message:"the current password is invalid please enter current pass"})
+    let isMatched = await compare(currentPassword, user.password);
+    if (!isMatched) {
+      return res
+        .status(400)
+        .json({
+          message: "the current password is invalid please enter current pass",
+        });
     }
 
-
     //hash new password
-    const newUserPassword=await hash(newPassword,12)
+    const newUserPassword = await hash(newPassword, 12);
     //replace current password of user with hashed new password
-    await UserModel.updateOne({ _id: req.user.id },
-        { $set: { password: newUserPassword }})
+    await UserModel.updateOne(
+      { _id: req.user.id },
+      { $set: { password: newUserPassword } },
+    );
     /*//save
     const user = await UserModel.findById(req.user._id)
     user.password = newUserPassword
       await user.save()*/
     //sand res
-    res.status(200).json({message:"password changed successfully"})
-})
+    res.status(200).json({ message: "password changed successfully" });
+  },
+);
